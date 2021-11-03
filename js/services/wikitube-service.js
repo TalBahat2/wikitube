2f6d1df28079e5ace3a8d27457a1dc6f57019dd6
TalBahat2 <tal.bahat2@gmail.com>
2021-11-03 07:23:38 +0200

    API trial

--- js/services/wikitube-service.js
+++ js/services/wikitube-service.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const API_KEY = 'AIzaSyAsSkeq8ZayoJWtIX9xirHwTU2UG_FVXc8';
+
 function search(txt) {
-    
+    return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${API_KEY}&q=$%7Bvalue`)
+        .then(res => console.log('res',res));
 }
