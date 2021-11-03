18c6880bdefd64b9ef8a9c0c8ec4a6e8c8c15d48
TalBahat2 <tal.bahat2@gmail.com>
2021-11-03 07:09:20 +0200

    basic css done

---
+++ js/wikitube-controller.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function init() {
+
+}
+
+function onSearch() {
+    const elInput = document.querySelector('[name=search-input');
+    const txt = elInput.value;
+    elInput.value = '';
+    if(!txt) return;
+    
+    search(txt);
+}
