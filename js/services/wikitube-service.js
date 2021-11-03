c96ea6c73a44352b55a10e548246e54e51ec9190
TalBahat2 <tal.bahat2@gmail.com>
2021-11-03 07:45:23 +0200

    trial2

--- js/services/wikitube-service.js
+++ js/services/wikitube-service.js
@@ -2,11 +2,11 @@
 
 const YOUTUBE_API_KEY = 'AIzaSyAsSkeq8ZayoJWtIX9xirHwTU2UG_FVXc8';
 
-function search(txt) {
-    console.log('hi');
-    return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YOUTUBE_API_KEY}&q=$%7Bvalue`)
-        .then(res => console.log('res', res));
-}
+// function search(txt) {
+//     console.log('hi');
+//     return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YOUTUBE_API_KEY}&q=$%7Bvalue`)
+//         .then(res => console.log('res', res));
+// }
 
 function searchWiki(txt) {
     return axios.get(`https://en.wikipedia.org/w/api.php?&origin=*&action=query&list=search&srsearch=${txt}&format=json`)
